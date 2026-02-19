ad61153b0b0a1663f992423cab0db4f93f80923c
santipxx <santivivivi@gmail.com>
2026-02-19 11:08:04 -0500

    Grupo de rutas

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,11 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/',HomeController::class);
 
-Route::get('/product',[ProductController::class,'index']);
 
-Route::get('/product/create',[ProductController::class,'create']);
-
+Route::prefix('product')->controller(ProductController::class)->group(function(){
+    Route::get('/','index');
+    Route::get('/create','create');
+    Route::get('/{producto}','show');
+});
 //rutas dinamicas
-Route::get('/product/{producto}',[ProductController::class,'show']);
+
